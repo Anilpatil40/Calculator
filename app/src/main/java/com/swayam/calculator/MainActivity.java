7baaa1384eea0048617b36a368e9d692d0cf632d
panil8993 <panil8993@gmail.com>
2021-01-02 10:16:21 +0530

    Initial commit and designed layout

---
+++ app/src/main/java/com/swayam/calculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.swayam.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void onNumberClick(View view){
+
+    }
+
+    public void onOperatorClick(View view){
+
+    }
+
+    public void onEqualClick(View view){
+
+    }
+
+    public void onCancelOrDeleteClick(View view){
+
+    }
+}
